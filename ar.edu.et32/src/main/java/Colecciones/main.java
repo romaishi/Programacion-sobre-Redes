ee45e1672c509d868096484eed3fce77db35a50c
romaishi <romanishida2602@gmail.com>
2025-04-09 12:48:21 -0300

    Update main.java

--- ar.edu.et32/src/main/java/Colecciones/main.java
+++ ar.edu.et32/src/main/java/Colecciones/main.java
@@ -1,9 +1,25 @@
 package Colecciones;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] vector = new int[5];
+		
+		//Crear una Lista
+		ArrayList<String> nombres = new ArrayList<>();
+		ArrayList comidas = new ArrayList<>();
+		List<Integer> numeros = new ArrayList<>();
+		
+		LinkedList<Integer> otraLista = new LinkedList<>();
+		
+		List<String> productos;
 
 	}
 
