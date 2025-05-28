ae2bacd2c283d36622298b1d54027c3bee374da6
romaishi <romanishida2602@gmail.com>
2025-05-28 11:52:55 -0300

    Archivos Basico
    Terminamos con los conceptos basicos de archivos

--- Files/src/main/java/Files/archivos.java
+++ Files/src/main/java/Files/archivos.java
@@ -1,49 +1,47 @@
 package Files;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class archivos {
-
+public class Archivos {
+	
 	PrintStream ps;
 	File file;
+	File filee;
 	
-	public archivos()
-	{
+	public Archivos(String ruta){
 		ps = new PrintStream(System.out);
-		//c:\\user\\gonza\\miArchivo.txt
-		file = new File("miArchivo.txt"); 
-		/*
-		file.createNewFile();
+		file = new File(ruta);
+		/**
+		file = new File("C:\\Users\\Redes-20\\Desktop\\PornHub.porno");
 		file.delete();
 		file.deleteOnExit();
 		file.exists();
 		file.getAbsoluteFile();
+		file.getAbsolutePath();
 		file.getName();
 		file.getParent();
+		file.getPath();
 		file.getTotalSpace();
+		file.isDirectory();
 		file.isHidden();
 		file.isFile();
-		file.isDirectory();
 		file.list();
 		file.listFiles();
 		file.mkdir();
-		file.length();
-		file.renameTo(new File("nuevoNombre.txt"));
-		*/
-		this.rutaFiles(file);
-		this.crearFileConPrintStreamEasy(file);
+		file.renameTo(file);
+		 */
 	}
-	
-	
-	
-	
-	
 	
 	/**
 	 * ESTE TEXTO NO TIENE NINGUNA ETIQUETA DE IDENTIFICACION. ESTO NO VA A
@@ -55,61 +53,119 @@ public class archivos {
 	 * @param f Este metodo recibe un archivo.
 	 * @see FlujoDeDatos.File.
 	 * @since v1.0
+	 * @exception
+	 * @throw
+	 * @return
+	 * @author Redes-20
 	 */
-	public void rutaFiles(File f)
+	public File getFiles()
 	{
-		
+		return this.file;
 	}
 
-	public void crearFileConPrintStreamEasy(File f) 
-	{
+	public void crearFileConPrintStreamEasy(File f) {
+		this.crearFileConPrintStreamEasy(file);
 		FileOutputStream fos = null;
 		PrintStream fs = null;
-		
+	
 		try {
-			fos = new FileOutputStream(f);
+			fos = new FileOutputStream(file);
 			fs = new PrintStream(fos);
 			
-			fs.print("Una linea");
-			fs.println("Nueva linea");
+			fs.print("Visiten Xvideos.com");
+			fs.println("Visiten PornHub.com");
 			fs.write('d');
-			fs.append("Consorti");
-			
+			fs.append(("Gonza gil"));
 			fs.flush();
 		} catch (FileNotFoundException e) {
-			Logger.getLogger(archivos.class.getName()).log(Level.WARNING,null,e);
+			// TODO Auto-generated catch block
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
 		} finally {
 			try {
-				if(fs != null)
+				if(fs != null) {
 					fs.close();
-
-				if(fos != null)
+				}
+				if(fos != null) {
 					fos.close();
-			} catch (IOException e){
-				Logger.getLogger(archivos.class.getName()).log(Level.WARNING,null,e);
+				}
+			} catch(IOException e) {
+				Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+			}
+		}
+	}//Fin crearFileConPrintStreamEasy
+	
+	public void crearFileConPrinter(File f) { // Mas optimizado
+		FileWriter fw = null; //usa buffered directo
+		PrintWriter pw = null;
+		try {
+			if(!f.exists()) {
+				try {
+					f.createNewFile();
+				}catch(IOException e) {
+					Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+				}
 			}
 			
-
+			fw = new FileWriter(f);
+			pw = new PrintWriter(fw);
+			
+			pw.print("Visiten Xvideos.com");
+			pw.print("Crearle una cuenta a MonsterCreator");
+			pw.println("Visiten PornHub.com");
+			pw.write('d');
+			pw.append(("Gonza genio"));
+			pw.flush();
+		}catch(FileNotFoundException e) {
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+		}catch(IOException e){
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+		}finally{
+			try{
+				if(pw != null) {
+					pw.close();
+				}
+				if(fw != null) {
+					fw.close();
+				}
+			}catch(IOException e) {
+				Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+			}
+		}//Finally
+		
+	}
+	
+	/**
+	*
+	* @param f
+	 * @return 
+	*/
+	public void crearFileConBuffer(File f, String texto) {
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(f , false ); //append
+			bw = new BufferedWriter(fw);
+			
+			//bw.append("ss");
+			//bw.write("s");
+			bw.append(texto);
+			bw.newLine();
+			bw.flush(); //Opcional Buffered
+		}catch(IOException e) {
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+		}finally {
+			try {
+				if(fw != null)
+					fw.close();
+				if(bw != null)
+					bw.close();
+			}catch(IOException e) {
+				Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+			}
 		}
 	}
 	
-	public void crearFileConPrinter(File f) 
-	{
-		
-	}
-
-	/**
-	*
-	* @param f
-	*/
-	public void crearFileConBuffer(File f) {
-	}
-	/**
-	*
-	* @param f
-	*/
-	public void crearFileConBuffer(File f) {
-	}
 	/**
 	 * Descripcion
 	 *
@@ -118,9 +174,71 @@ public class archivos {
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public String LeerFileConBuffer(File f) throws FileNotFoundException, IOException {
+	public String LeerFileConBuffer(File f) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(f);
+			br = new BufferedReader( fr );
+			
+			String line = "", texto = "";
+			while(  (line = br.readLine() ) != null )
+			{
+				texto.concat( line.concat("\n")  );
+			}
+			
+			return texto;
+		} catch (FileNotFoundException e) {
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING, null, e);
+		} catch (IOException e) {
+			Logger.getLogger(Archivos.class.getName()).log(Level.WARNING, null, e);
+		}finally {
+			try {
+				if( fr != null)
+					fr.close();
+				if( br != null)
+					br.close();
+			}catch(IOException e) {
+				Logger.getLogger(Archivos.class.getName()).log(Level.WARNING, null, e);
+			}
+		}
+		
+		return null;
 	}
-
-	public void leerFileCaracterAcaractet(File f) {
+	public String leerFileCaracterCaracter(File f) {
+		
+		FileReader fr = null;
+		
+		try
+		{
+			fr = new FileReader(f);
+			
+			int car, EOF = -1;
+			String texto = "";
+			
+			while((car = fr.read()) != EOF) {
+			
+				if (car == '\n') {					
+					texto.concat("\n");
+				}else {					
+					texto.concat(String.valueOf(car));
+				}
+				
+			}
+			return texto;
+		}catch (IOException e){
+			
+		}finally {
+			try {
+				if(fr != null)
+					fr.close();
+			}catch(IOException e) {
+				Logger.getLogger(Archivos.class.getName()).log(Level.WARNING,null,e);
+			}
+		}
+		return null;
+		
 	}
+	
 }
--- Files/src/main/java/Files/main.java
+++ Files/src/main/java/Files/main.java
@@ -4,7 +4,10 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Archivos arch = new Archivos("consorti.txt");
+		
+		arch.crearFileConBuffer(arch.getFiles(), "hola mundo");
+		
 	}
 
 }
