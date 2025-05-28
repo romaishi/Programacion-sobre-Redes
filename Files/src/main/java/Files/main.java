fa591618cd6fcc8fafa6e4c8ab2ff2f6f105a3df
romaishi <romanishida2602@gmail.com>
2025-05-28 12:46:22 -0300

    Basico Archivos
    Me olvide de guardar estos archivos antes de commitear

--- Files/src/main/java/Files/main.java
+++ Files/src/main/java/Files/main.java
@@ -1,12 +1,17 @@
 package Files;
 
+import java.io.PrintStream;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PrintStream ps = new PrintStream(System.out);
 		Archivos arch = new Archivos("consorti.txt");
 		
 		arch.crearFileConBuffer(arch.getFiles(), "hola mundo");
+		
+		arch.LeerFileConBuffer(arch.getFiles());
 		
 	}
 
